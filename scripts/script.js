0435505aeeb1daa4d3811312401fe34ad76b52f2
AzatBaisynov <azat.baisynov@gmail.com>
2022-02-13 22:56:11 +0600

    bugs

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+// carousel section
 $(document).ready(function(){
     $('.customer-logos').slick({
         slidesToShow: 4,
@@ -10,13 +11,53 @@ $(document).ready(function(){
         responsive: [{
             breakpoint: 768,
             settings: {
-                slidesToShow: 3
+                slidesToShow: 2
             }
         }, {
             breakpoint: 520,
             settings: {
-                slidesToShow: 2
+                slidesToShow: 1
             }
         }]
     });
 });
+// carousel section
+
+const testerBtn = document.querySelector("#tester")
+const testerTask = document.querySelector("#tester__task")
+const testerTitleNone = document.querySelector(".jobs__tester-title")
+
+const autoTesterBtn = document.querySelector("#auto__tester")
+const autoTesterTask = document.querySelector("#auto__testerTask")
+const autoTitleNone = document.querySelector(".jobs__auto-title")
+const jobsSkyButton = document.querySelector(".jobs__sky-button")
+const jobsBackImg = document.querySelector('.jobs__background-img')
+const jobsLink = document.querySelector(".jobs__link")
+
+
+testerBtn.onclick = () =>{
+    testerTask.style.display = "flex"
+    testerBtn.style.display = "none"
+    jobsSkyButton.style.display = "none"
+    jobsLink.style.display = 'block'
+}
+testerTitleNone.onclick = () =>{
+    testerBtn.style.display = "block"
+    testerTask.style.display = "none"
+    jobsSkyButton.style.display = "block"
+    jobsLink.style.display = 'none'
+}
+autoTesterBtn.onclick = () =>{
+    autoTesterBtn.style.display = "none"
+    autoTesterTask.style.display = "flex"
+    jobsSkyButton.style.display = "none"
+    jobsBackImg.style.display = "none"
+    jobsLink.style.display = 'block'
+}
+autoTitleNone.onclick = () =>{
+    autoTesterBtn.style.display = "block"
+    autoTesterTask.style.display = "none"
+    jobsSkyButton.style.display = "block"
+    jobsBackImg.style.display = "block"
+    jobsLink.style.display = 'none'
+}
